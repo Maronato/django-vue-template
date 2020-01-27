ec372184ff1dd11c883c1576953c2892e407a737
Maronato <gustavomaronato@gmail.com>
2020-01-27 17:17:58 -0500

    removed unused httpresponse

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.http import HttpResponse
 from django.views import View
 
 from rest_framework_simplejwt.views import TokenRefreshView
